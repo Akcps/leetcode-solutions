fb20a56f63e3d4daf48468c7f6b06aaa5917b157
akcps <ajitkumarjsr15@gmail.com>
2019-12-19 00:15:50 +0530

    2. Add Two Numbers

---
+++ 2-AddTwoNumbers/Solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+/**
+ * Runtime: 2 ms, faster than 77.68% of Java online submissions for Add Two Numbers.
+ * Memory Usage: 45.2 MB, less than 78.06% of Java online submissions for Add Two Numbers.
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+}
