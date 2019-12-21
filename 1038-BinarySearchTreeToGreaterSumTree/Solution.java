e5ff37245f8028d18bd87141107aa80da6a768cf
akcps <ajitkumarjsr15@gmail.com>
2019-12-21 09:04:12 +0530

    1038. Binary Search Tree to Greater Sum Tree

---
+++ 1038-BinarySearchTreeToGreaterSumTree/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/**
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Search Tree to Greater Sum Tree.
+ * Memory Usage: 34.2 MB, less than 100.00% of Java online submissions for Binary Search Tree to Greater Sum Tree.
+ */
+class Solution {
+    int sum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if (root != null) {
+            bstToGst(root.right);
+            sum += root.val;
+            root.val = sum;
+            bstToGst(root.left);
+        }
+        return root;
+    }
+}
