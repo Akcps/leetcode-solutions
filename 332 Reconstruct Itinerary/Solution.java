6b0a4314644fb32ff5b67a814a8aca05c35014c9
akcps <ajitkumarjsr15@gmail.com>
2019-12-31 17:53:22 +0530

    332. Reconstruct Itinerary

--- 332 Reconstruct Itinerary/Solution.java
+++ 332 Reconstruct Itinerary/Solution.java
@@ -1,6 +1,6 @@
 /**
- * Runtime: 5 ms, faster than 80.48% of Java online submissions for Reconstruct Itinerary.
- * Memory Usage: 43.6 MB, less than 64.18% of Java online submissions for Reconstruct Itinerary.
+ * Runtime: 4 ms, faster than 99.98% of Java online submissions for Reconstruct Itinerary.
+ * Memory Usage: 43.5 MB, less than 64.18% of Java online submissions for Reconstruct Itinerary.
  */
 class Solution {
 
@@ -14,16 +14,16 @@ class Solution {
             graph.put(ticket.get(0), cities);
         }
 
-        List<String> result = new LinkedList<String>();
+        LinkedList<String> result = new LinkedList<String>();
         dfs("JFK", result);
         return result;
     }
 
-    private void dfs(String city, List<String> result) {
+    private void dfs(String city, LinkedList<String> result) {
         PriorityQueue<String> cities = graph.get(city);
         while(cities != null && !cities.isEmpty()) {
             dfs(cities.poll(), result);
         }
-        result.add(0, city);
+        result.addFirst(city);
     }
 }
