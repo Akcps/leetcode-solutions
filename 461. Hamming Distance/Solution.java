a4b28c2e46496bc3aa4af65f28097f54c261e45e
akcps <ajitkumarjsr15@gmail.com>
2020-01-05 11:18:23 +0530

    461. Hamming Distance

---
+++ 461. Hamming Distance/Solution.java
@@ -0,0 +1,17 @@
+/**
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Hamming Distance.
+ * Memory Usage: 33.1 MB, less than 5.09% of Java online submissions for Hamming Distance.
+ */
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int xor = x ^ y;
+        int dist = 0;
+        while (xor != 0) {
+            if(xor % 2 == 1) {
+                dist++;
+            }
+            xor = xor >> 1;
+        }
+        return dist;
+    }
+}
