2288b37a46104dcbbee2205a75fedd66b70caddc
akcps <ajitkumarjsr15@gmail.com>
2020-01-15 10:23:46 +0530

    814. Binary Tree Pruning

---
+++ 814. Binary Tree Pruning/Solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/**
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Tree Pruning.
+ * Memory Usage: 35.3 MB, less than 100.00% of Java online submissions for Binary Tree Pruning.
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.val == 0 && root.left == null && root.right == null) {
+            return null;
+        }
+        return root;
+    }
+}
