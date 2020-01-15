7aacf092f7f689293fbc80a2b056906f3e8907d1
akcps <ajitkumarjsr15@gmail.com>
2020-01-15 10:16:33 +0530

    41. First Missing Positive

---
+++ 41. First Missing Positive/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for First Missing Positive.
+ * Memory Usage: 35.2 MB, less than 100.00% of Java online submissions for First Missing Positive.
+ */
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if(nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]){
+                int j = nums[i]-1;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            } else {
+                i++;
+            }
+        }
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length+1;
+    }
+}
