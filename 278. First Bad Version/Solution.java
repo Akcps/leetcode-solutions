2bd84245213769b8f807129a743175edacbdbebd
akcps <ajitkumarjsr15@gmail.com>
2020-01-28 21:36:32 +0530

    278. First Bad Version

---
+++ 278. First Bad Version/Solution.java
@@ -0,0 +1,31 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+/**
+ * Runtime: 16 ms, faster than 15.32% of Java online submissions for First Bad Version.
+ * Memory Usage: 36.1 MB, less than 5.63% of Java online submissions for First Bad Version.
+ */
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            boolean flag = isBadVersion(mid);
+            if (!flag) {
+                low = mid + 1;
+            } else {
+                if (mid - 1 >= 0) {
+                    boolean prev = isBadVersion(mid - 1);
+                    if (!prev) {
+                        return mid;
+                    }
+                }
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+
+}
