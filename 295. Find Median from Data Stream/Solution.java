63938c8060fbeb86f885f385355dd0f34df872a3
akcps <ajitkumarjsr15@gmail.com>
2020-02-06 23:08:37 +0530

    295. Find Median from Data Stream

---
+++ 295. Find Median from Data Stream/Solution.java
@@ -0,0 +1,42 @@
+/**
+ * Runtime: 44 ms, faster than 97.24% of Java online submissions for Find Median from Data Stream.
+ * Memory Usage: 55.3 MB, less than 100.00% of Java online submissions for Find Median from Data Stream.
+ */
+class MedianFinder {
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        minHeap = new PriorityQueue<Integer>();
+        maxHeap = new PriorityQueue<Integer>((p1,p2)->p2-p1);
+    }
+
+    public void addNum(int num) {
+        if (maxHeap.size() == 0 || maxHeap.peek() > num) {
+            maxHeap.add(num);
+        } else {
+            minHeap.add(num);
+        }
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.add(maxHeap.poll());
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.add(minHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (maxHeap.size() > minHeap.size()) {
+            return maxHeap.peek();
+        } else {
+            return (maxHeap.peek() + minHeap.peek())/2.0;
+        }
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
