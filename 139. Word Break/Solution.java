5428ff8a2e70ef347fd19e93b32f2af805b36fa7
akcps <ajitkumarjsr15@gmail.com>
2020-02-08 00:12:47 +0530

    139. Word Break

---
+++ 139. Word Break/Solution.java
@@ -0,0 +1,21 @@
+/**
+ * Runtime: 6 ms, faster than 46.51% of Java online submissions for Word Break.
+ * Memory Usage: 41.5 MB, less than 5.08% of Java online submissions for Word Break.
+ */
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+}
