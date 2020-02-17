d4be896973fce8a1b0d85e97f199a8f93c1066ee
akcps <ajitkumarjsr15@gmail.com>
2020-02-17 18:39:27 +0530

    278. First Bad Version

--- 278. First Bad Version/Solution.java
+++ 278. First Bad Version/Solution.java
@@ -29,3 +29,25 @@ public class Solution extends VersionControl {
 
 
 }
+
+/**
+ * Runtime: 12 ms, faster than 56.50% of Java online submissions for First Bad Version.
+ * Memory Usage: 36.4 MB, less than 5.63% of Java online submissions for First Bad Version.
+ */
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (isBadVersion(mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+
+}
