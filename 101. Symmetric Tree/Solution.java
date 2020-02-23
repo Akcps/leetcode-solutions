c089ef4a144b5e4245167b26427f874c58c550a8
akcps <ajitkumarjsr15@gmail.com>
2020-02-23 19:32:20 +0530

    101. Symmetric Tree

---
+++ 101. Symmetric Tree/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+/**
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Symmetric Tree.
+ * Memory Usage: 37.8 MB, less than 72.11% of Java online submissions for Symmetric Tree.
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if ((t1 == null && t2!=null) || (t2==null && t1!=null)){
+            return false;
+        }
+        return t1.val == t2.val && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+}
