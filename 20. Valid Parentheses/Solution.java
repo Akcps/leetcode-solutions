2f2e0925e4bfed90247ef69c0599fbb4e5def061
akcps <ajitkumarjsr15@gmail.com>
2020-03-26 18:38:48 +0530

    20. Valid Parentheses

--- 20. Valid Parentheses/Solution.java
+++ 20. Valid Parentheses/Solution.java
@@ -27,8 +27,34 @@ class Solution {
             if (ch[i] =='(' || ch[i]=='{' || ch[i] =='[')
                 stack.push(ch[i]);
         }
-        if (!stack.isEmpty())
-            return false;
-        return true;
+        return stack.isEmpty();
+    }
+}
+
+/**
+ * Runtime: 1 ms, faster than 98.66% of Java online submissions for Valid Parentheses.
+ * Memory Usage: 37.8 MB, less than 5.06% of Java online submissions for Valid Parentheses.
+ */
+class Solution {
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0)
+            return true;
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        Stack<Character> stack = new Stack<Character>();
+        char[] ch = s.toCharArray();
+        for(int i= 0; i< ch.length; i++) {
+            if (map.containsKey(ch[i]))  {
+                if (!stack.isEmpty() && stack.peek() == map.get(ch[i]))
+                    stack.pop();
+                else
+                    return false;
+            }
+            if (ch[i] =='(' || ch[i]=='{' || ch[i] =='[')
+                stack.push(ch[i]);
+        }
+        return stack.isEmpty();
     }
 }
