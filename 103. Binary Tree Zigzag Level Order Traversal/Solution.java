a0c7a0e8aa2761df5da5156a221596c95fa4d835
akcps <ajitkumarjsr15@gmail.com>
2021-06-16 10:39:00 +0530

    103. Binary Tree Zigzag Level Order Traversal

--- 103. Binary Tree Zigzag Level Order Traversal/Solution.java
+++ 103. Binary Tree Zigzag Level Order Traversal/Solution.java
@@ -43,3 +43,69 @@ class Solution {
         return res;
     }
 }
+
+/**
+ * Solution based on 2 stacks
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/**
+ * Runtime: 1 ms, faster than 75.24% of Java online submissions for Binary Tree Zigzag Level Order Traversal.
+ * Memory Usage: 38.7 MB, less than 87.02% of Java online submissions for Binary Tree Zigzag Level Order Traversal.
+ */
+class Solution {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        Stack<TreeNode> tempStack = new Stack<>();
+        tempStack.push(root);
+        boolean leftToRight = true;
+        while(!tempStack.isEmpty()) {
+            int size = tempStack.size();
+            List<Integer> currentList = new ArrayList<>();
+            stack = tempStack;
+            tempStack = new Stack<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = stack.pop();
+                currentList.add(node.val);
+                if (!leftToRight) {
+                    if (node.right != null) {
+                        tempStack.push(node.right);
+                    }
+                    if (node.left != null) {
+                        tempStack.push(node.left);
+                    }
+                } else {
+                    if (node.left != null) {
+                        tempStack.push(node.left);
+                    }
+                    if (node.right != null) {
+                        tempStack.push(node.right);
+                    }
+                }
+            }
+            result.add(currentList);
+            leftToRight = !leftToRight;
+
+        }
+        return result;
+    }
+}
