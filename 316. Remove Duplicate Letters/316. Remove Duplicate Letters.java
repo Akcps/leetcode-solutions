25146579ac247504d8187d53bbe389c54881889b
akcps <ajitkumarjsr15@gmail.com>
2021-06-19 21:53:31 +0530

    316. Remove Duplicate Letters

---
+++ 316. Remove Duplicate Letters/316. Remove Duplicate Letters.java
@@ -0,0 +1,33 @@
+/**
+ * Runtime: 4 ms, faster than 50.15% of Java online submissions for Remove Duplicate Letters.
+ * Memory Usage: 39.2 MB, less than 37.90% of Java online submissions for Remove Duplicate Letters.
+ * Time Complexity: O(n)
+ * Space Complexity: O(1) - As the space can be maximum of 26 (characters)
+ */
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        HashSet existsInSolution = new HashSet<>();
+        Map<Character, Integer> lastOccurrence = new HashMap<>();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length();  i++) {
+            lastOccurrence.put(s.charAt(i), i);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            while (!existsInSolution.contains(ch)) {
+                while (!stack.isEmpty() && ch < stack.peek() && lastOccurrence.get(stack.peek()) > i) {
+                    existsInSolution.remove(stack.pop());
+                }
+                existsInSolution.add(ch);
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder result = new StringBuilder(stack.size());
+        for (Character ch : stack) {
+            result.append(ch.charValue());
+        }
+        return result.toString();
+    }
+}
