11887699beeb0f89560872cc59f0e7e33635347b
akcps <ajitkumarjsr15@gmail.com>
2021-07-15 10:57:31 +0530

    791. Custom Sort String

--- 791. Custom Sort String/791. Custom Sort String.java
+++ 791. Custom Sort String/791. Custom Sort String.java
@@ -1,4 +1,34 @@
 /**
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Custom Sort String.
+ * Memory Usage: 37.1 MB, less than 76.53% of Java online submissions for Custom Sort String.
+ * Time Complexity: O (n)
+ * Space Complexity: O (n)
+ */
+class Solution {
+    public String customSortString(String order, String str) {
+        int[] count = new int[26];
+        for (char ch: str.toCharArray()) {
+            count[ch - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char ch: order.toCharArray()) {
+            if (count[ch - 'a'] > 0) {
+                for (int i = 0; i < count[ch - 'a']; i++) {
+                    sb.append(ch);
+                }
+                count[ch - 'a'] = 0;
+            }
+        }
+        for (int j = 0 ; j < 26; j++) {
+            for (int i = 0; i < count[j]; i++) {
+                sb.append((char)(j +'a'));
+            }
+        }
+        return sb.toString();
+    }
+}
+
+/**
  * Runtime: 2 ms, faster than 45.57% of Java online submissions for Custom Sort String.
  * Memory Usage: 37 MB, less than 76.08% of Java online submissions for Custom Sort String.
  * Time Complexity: O (n)
