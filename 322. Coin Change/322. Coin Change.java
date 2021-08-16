5ae55c81ed0901a0c5e7403ec01b2a2219fe8680
akcps <ajitkumarjsr15@gmail.com>
2021-08-16 16:49:49 +0530

    322. Coin Change

--- 322. Coin Change/322. Coin Change.java
+++ 322. Coin Change/322. Coin Change.java
@@ -1,22 +1,22 @@
 /**
  * Runtime: 13 ms, faster than 51.38% of Java online submissions for Coin Change.
  * Memory Usage: 35.8 MB, less than 97.63% of Java online submissions for Coin Change.
+ * Time Complexity: O (N ^ 2)
+ * Space Complexity: O (N)
  */
 class Solution {
+
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int[] dp = new int[amount+1];
+        int[] dp = new int[amount + 1];
         Arrays.fill(dp, Integer.MAX_VALUE - 1);
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i >= coins[j]) {
-                    dp[i] = Math.min(dp[i], dp[i- coins[j]] + 1);
+            for (int coin : coins) {
+                if (i >= coin) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-        if (dp[amount] == Integer.MAX_VALUE - 1)
-            return -1;
-        return dp[amount];
+        return dp[amount] == Integer.MAX_VALUE - 1 ? -1 : dp[amount];
     }
 }
