63f14f8bdb45d3f1f5d4ef73d774394558126963
akcps <ajitkumarjsr15@gmail.com>
2021-09-01 12:01:16 +0530

    62. Unique Paths

--- 62. Unique Paths/Solution.java
+++ 62. Unique Paths/Solution.java
@@ -4,20 +4,24 @@
  */
 class Solution {
     public int uniquePaths(int m, int n) {
-        if (m == 0 || n == 0)
+        if (m == 0 || n == 0) {
             return 0;
+        }
         int[][] paths = new int[m][n];
-        for (int i = 0; i < m; i++)
+        for (int i = 0; i < m; i++) {
             paths[i][0] = 1;
-        for (int i = 0; i < n; i++)
+        }
+
+        for (int i = 0; i < n; i++) {
             paths[0][i] = 1;
+        }
 
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                paths[i][j] = paths[i-1][j] + paths[i][j-1];
+                paths[i][j] = paths[i - 1][j] + paths[i][j - 1];
             }
         }
 
-        return paths[m-1][n-1];
+        return paths[m - 1][n - 1];
     }
 }
