eff3f7506164c9fd3a03e429cb2f87f5a39c16a1
akcps <ajitkumarjsr15@gmail.com>
2021-09-09 10:30:39 +0530

    189. Rotate Array

---
+++ 189. Rotate Array/189. Rotate Array.java
@@ -0,0 +1,24 @@
+/**
+ * Runtime: 1 ms, faster than 65.33% of Java online submissions for Rotate Array.
+ * Memory Usage: 64 MB, less than 11.83% of Java online submissions for Rotate Array.
+ */
+
+class Solution {
+
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
