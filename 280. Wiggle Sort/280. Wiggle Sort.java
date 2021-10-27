74898ba4cc49b85d4cf35abd1e7ad4470c2e7b5d
akcps <ajitkumarjsr15@gmail.com>
2021-10-27 18:56:04 +0530

    280. Wiggle Sort

---
+++ 280. Wiggle Sort/280. Wiggle Sort.java
@@ -0,0 +1,28 @@
+/**
+ * Runtime: 0 ms, faster than 100.00% of Java online submissions for Wiggle Sort.
+ * Memory Usage: 40.3 MB, less than 48.02% of Java online submissions for Wiggle Sort.
+ */
+
+class Solution {
+    public void wiggleSort(int[] nums) {
+        boolean less = true;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (less) {
+                if (nums[i] > nums[i + 1]) {
+                    swap(nums, i, i + 1);
+                }
+            } else {
+                if (nums[i] < nums[i + 1]) {
+                    swap(nums, i, i + 1);
+                }
+            }
+            less = !less;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
