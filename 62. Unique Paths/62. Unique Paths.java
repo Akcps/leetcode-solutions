fb580c90174c9691fc923d6a795c3de170d5e6cd
akcps <ajitkumarjsr15@gmail.com>
2021-11-17 15:48:40 +0530

    62. Unique Paths

--- 62. Unique Paths/62. Unique Paths.java
+++ 62. Unique Paths/62. Unique Paths.java
@@ -25,3 +25,23 @@ class Solution {
         return paths[m - 1][n - 1];
     }
 }
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] moves = new int[m][n];
+        int[] dx = new int[]{0, 1};
+        int[] dy = new int[]{1, 0};
+        moves[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0) {
+                    moves[i][j] += moves[i - 1][j];
+                }
+                if (j > 0) {
+                    moves[i][j] += moves[i][j - 1];
+                }
+            }
+        }
+        return moves[m - 1][n - 1];
+    }
+}
