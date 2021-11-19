73f76d2021d55e40ef87458f0143f251fbfa89eb
akcps <ajitkumarjsr15@gmail.com>
2021-11-19 12:41:19 +0530

    461. Hamming Distance

--- 461. Hamming Distance/461. Hamming Distance.java
+++ 461. Hamming Distance/461. Hamming Distance.java
@@ -7,7 +7,7 @@ class Solution {
         int xor = x ^ y;
         int dist = 0;
         while (xor != 0) {
-            if(xor % 2 == 1) {
+            if (xor % 2 == 1) {
                 dist++;
             }
             xor = xor >> 1;
