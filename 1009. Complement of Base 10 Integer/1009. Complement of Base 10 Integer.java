38fd45826361cbad122b5b3f570a2806cec64694
akcps <ajitkumarjsr15@gmail.com>
2022-01-04 23:08:25 +0530

    1009. Complement of Base 10 Integer

---
+++ 1009. Complement of Base 10 Integer/1009. Complement of Base 10 Integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int bitwiseComplement(int N) {
+        if (N == 0) {
+            return 1;
+        }
+        int todo = N, bit = 1;
+        while (todo != 0) {
+            N = N ^ bit;
+            bit = bit << 1;
+            todo = todo >> 1;
+        }
+        return N;
+    }
+
+}
