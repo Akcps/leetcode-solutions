4b0b85e5b786a5d16db8098323fa8bdd9d0bff44
Ajit Kumar <ajitkumarjsr15@gmail.com>
2022-06-27 08:58:39 +0530

    Time: 30 ms (5.22%), Space: 49.4 MB (6.60%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.java
@@ -0,0 +1,27 @@
+class Solution {
+    int[] r = new int[] { -1, 0, 0, 1 }; 
+    int[] c = new int[] { 0, -1, 1, 0 };
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int oldColor = image[sr][sc];
+        if (oldColor == color) {
+            return image;
+        }
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {sr,sc});
+        while(!queue.isEmpty()) {
+           int[] current = queue.poll();
+           image[current[0]][current[1]] = color;
+            for (int i = 0; i < 4; i++) {
+                int newRow = current[0] + r[i];
+                int newCol = current[1] + c[i];
+                System.out.println(newRow);
+                System.out.println(newCol);
+                if (newRow >= 0 && newCol >= 0 && newRow < image.length && newCol < image[0].length && image[newRow][newCol] == oldColor) {
+                    queue.add(new int[] {newRow,newCol});
+                }
+            }
+        }
+        return image;
+        
+    }
+}
