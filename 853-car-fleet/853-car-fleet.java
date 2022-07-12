fc731b79fcd4beb82e8589faea030884205a761c
Ajit Kumar <ajitkumarjsr15@gmail.com>
2022-07-12 22:55:11 +0530

    Time: 274 ms (8.75%), Space: 96 MB (33.50%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        List<Pair<Integer, Integer>> positionSpeedPairs = new ArrayList<>();
+        for (int i = 0; i < position.length; i++) {
+            positionSpeedPairs.add(new Pair(position[i], speed[i]));
+        }
+        Collections.sort(positionSpeedPairs, (a, b) -> b.getKey() - a.getKey());
+        
+        
+        Stack<Double> stack = new Stack<>();
+        
+        for(Pair<Integer,Integer> p: positionSpeedPairs) {
+            double val = (target - p.getKey()) / (double)p.getValue();
+            if (stack.size() == 0) {
+               stack.add(val); 
+            } else if (stack.size() >= 1 && val > stack.peek()) {
+                stack.add(val);
+            }
+        }
+        return stack.size();
+        
+        
+    }
+}
