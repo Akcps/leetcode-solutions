83b2d827dd10a532d140d792f3fd94715996f296
Ajit Kumar <ajitkumarjsr15@gmail.com>
2022-08-17 00:20:11 +0530

    Time: 481 ms (13.02%), Space: 22.2 MB (29.00%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -6,8 +6,8 @@ class Solution:
             left[i + 1] = left[i] * nums[i]
             right[len(nums) - i - 1] = right[len(nums) - i] * nums[len(nums) - i - 1]
         
-        res = [1] * len(nums) 
+        res = list()
         for i in range(len(nums)):
-            res[i] = left[i] * right[i+1]
+            res.append(left[i] * right[i+1])
         
         return res
