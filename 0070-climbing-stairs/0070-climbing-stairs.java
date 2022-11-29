3437a52225a7f0aa3d86ab80ef591776ae9ed570
Ajit Kumar <ajitkumarjsr15@gmail.com>
2022-11-29 19:22:45 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (67.77%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        
+        if (n == 2) {
+            return 2;
+        }
+        
+        int a = 1;
+        int b = 2;
+        for(int i = 3; i <= n; i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        
+        return b;
+    }
+}
