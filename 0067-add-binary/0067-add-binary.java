9f2c4e54bd1528399d9cf490b059dadc51ae4ac9
Ajit Kumar <ajitkumarjsr15@gmail.com>
2023-02-14 21:08:26 +0530

    Time: 3 ms (47.51%), Space: 42.1 MB (57.05%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,30 +1,40 @@
 class Solution {
-public String addBinary(String a, String b) {
-    
-    Stack<Integer> stack = new Stack<>();
-    int carry = 0;
-    int n = a.length() - 1;
-    int m = b.length() - 1;
-
-    while(n >= 0 || m >= 0){
+    public String addBinary(String a, String b) {
         
-        int sum = carry;
-
-        if(n >= 0) sum += a.charAt(n) - 48;
-        if(m >= 0) sum += b.charAt(m) - 48;
-
-        carry = sum/2;
-        stack.push(sum % 2);
-
-        n--;
-        m--;
-    }
-    if(carry == 1) stack.push(carry);
-
-    String answer = "";
-    while(!stack.isEmpty()){
-        answer += stack.pop();
-    }
-    return answer;
+        int index = 0;
+        Stack<Integer> stack = new Stack<>();
+        int carry = 0;
+        
+        while (index < Math.max(a.length(), b.length())) {
+            int aChar = 0;
+            int bChar = 0;
+            
+            if (index < a.length()) {
+              aChar = a.charAt(a.length() - index - 1) - 48;
+            }
+            
+            if (index < b.length()) {
+              bChar = b.charAt(b.length() - index - 1) - 48;
+            }
+            
+            int sum = aChar + bChar + carry;
+            
+            carry = sum / 2;
+            
+            stack.push(sum % 2);
+            
+            index++;
+        }
+        
+        if (carry == 1) {
+            stack.push(carry);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()){
+            int c = stack.pop();
+            sb.append(c);
+        }
+        return sb.toString();
     }
 }
